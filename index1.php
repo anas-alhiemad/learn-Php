3e489dd6f02b701f1775df119d515c2b87c9f149
anas <anasalhiemad@gmail.com>
2024-01-10 11:14:01 -0800

    Comment in Php

---
+++ index1.php
@@ -0,0 +1,18 @@
+<?php 
+
+// this is single line comment 
+
+echo 'test for comment ';
+
+/**
+ * this is multiple line comment 
+ * 
+ * 
+ * 
+ */
+echo 'test for multiple line comment ';
+
+
+
+
+?>
