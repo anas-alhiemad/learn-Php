d2c3be73f2e7bcdafddad6cc87e502705e667988
anas <anasalhiemad@gmail.com>
2024-01-11 12:22:31 -0800

    Print array  in Php

---
+++ index8.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 
+ * Data Type 
+ * ==================
+ * ARRAY
+ * print_r();
+ */
+
+ echo '<pre>';
+   print_r([
+            "ahmed",
+            "a" => "anas",
+            "b" => "belal",
+            "nour",
+            6 => "mohammad",
+            "omer",
+            "names" => ["anas0","anas1","anas3"],
+
+
+   ]); 
+ echo'</pre>';
+
+
+?>
