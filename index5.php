133c045554e02e007806988861b45f36de46b74a
anas <anasalhiemad@gmail.com>
2024-01-11 01:00:11 -0800

    Converting to boolean

---
+++ index5.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * Boolean 
+ * converting to boolean 
+ * 
+ * var_dump
+ */
+var_dump((bool) "");
+echo '<br>';
+var_dump((bool) 0);
+echo '<br>';
+var_dump((bool) array());
+echo '<br>';
+var_dump((bool) []);
+echo '<br>';
+var_dump((bool) "0");
+echo '<br>';
+var_dump((bool) "anas");
+echo '<br>';
+var_dump((bool) [1]);
+echo '<br>';
+var_dump((bool) array("anas"));
+echo '<br>';
+var_dump((bool) 2);
+echo '<br>';
+
+
+?>
