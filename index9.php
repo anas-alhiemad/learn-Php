e280e0b7a29e5e4a7833236fe29d7304b70e10c3
anas <anasalhiemad@gmail.com>
2024-01-15 08:50:25 -0800

    Variable  in Php

---
+++ index9.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Variables
+ *  Name Rule
+ * 1 Start with dollar sign $
+ * 2 Start with a letter [a-z] or [A-Z] or underscore
+ * 3 Can use number inside the name 
+ * 4 No special characters allowed
+ * 5 Case sensitive
+ * 
+ * 
+ * search loosely typed language
+ */
+
+ $userName = "Anas Alhiemad";
+
+ $UserName = "anas ali";
+
+ echo $userName;
+ echo "<br>";
+ echo $UserName;
+ echo"<br>";
+ echo "MY Name Is : $userName";  // As Variable 
+ echo"<br>";
+ echo 'MY Name Is : $userName';  // As String 
+
+?>
