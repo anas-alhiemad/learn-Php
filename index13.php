ef523915f6adbf1fc18ea31077d5ec1568908098
anas <anasalhiemad@gmail.com>
2024-01-15 11:44:43 -0800

    Constants  in Php

---
+++ index13.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * 
+ * constants
+ * that value cannot change during the execution 
+ * constants always upperCase 
+ */
+
+
+ define("DB_NAME","DATA");
+ define("NUMBER","21");
+
+
+ echo DB_NAME;
+ echo "<br>";
+ echo NUMBER;
+
+
+
+
+?>
---
+++ index14.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * 
+ * Pre_defined constants "here case sensitive "
+ * PHP_VERSION
+ * PHP_OS_FAMILY
+ * 
+ * magic constants "case InSensitive" 
+ * __LINE__
+ * __FILE__
+ * __DIR__
+ * 
+ * reserved keyWord
+ * as  clone 
+ * as break
+ * 
+ * search 
+ * php PreDefined Constant in php
+ * compile time VS Runtime 
+ * list of reserved Words
+ */
+
+
+echo php_uname();
+echo '<br>';
+echo __FILE__;
+echo '<br>';
+echo PHP_VERSION;
+echo '<br>';
+?>
